37386139af8bfdaa9bdb4e9f68aed20ec5bf6f7d
Julia Venesia <jvenesiamenna@gmail.com>
2026-08-14 09:19:56 -0300

    hasta etapa3

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿//etapa 1 
+string name = "KIOSKO EL RECREO";
+Console.WriteLine(name);
+
+Console.WriteLine("Ingrese su nombre: " );
+string user = Console.ReadLine();
+Console.WriteLine($"Bienvenido, {user}. La Caja esta abierta.");
+Console.ReadLine();
+
+//etapa 2 
+Console.WriteLine("Ingrese el nombre del prodocuto: ");
+string product = Console.ReadLine();
+Console.WriteLine("Ingrese el precio del producto: ");
+decimal price = decimal.Parse(Console.ReadLine());  
+
+Console.WriteLine($"Producto cargado: {product} - Precio: ${price}");
+Console.ReadLine();
+
+//etapa 3 
+decimal total = 0;
+int cantidadProductos = 0;
+int opcion;
+
+do
+{
+    Console.WriteLine("¿Qué desea hacer?");
+    Console.WriteLine("1 - Cargar un producto");
+    Console.WriteLine("2 - Cerrar la venta");
+
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            Console.Write("Ingrese el nombre del producto: ");
+            string nombreProducto = Console.ReadLine();
+
+            Console.Write("Ingrese el precio del producto: ");
+            decimal precioProducto = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Producto cargado: {nombreProducto} - Precio: ${precioProducto}");
+
+            total += precioProducto;
+            cantidadProductos++;
+
+            break;
+
+        case 2:
+            Console.WriteLine($"Cantidad de productos: {cantidadProductos}");
+            Console.WriteLine($"Total de la venta: ${total}");
+            break;
+
+        default:
+            Console.WriteLine("Opción inválida.");
+            break;
+    }
+
+}
+while (opcion != 2);
+
+
+
+
+
+
+
