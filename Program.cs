1b6b70241c1f38d5446425491c0d41b5672e8cda
Julia Venesia <jvenesiamenna@gmail.com>
2026-08-14 10:22:42 -0300

    etapa 5: medio de pago

--- Program.cs
+++ Program.cs
@@ -16,9 +16,12 @@ decimal price = decimal.Parse(Console.ReadLine());
 Console.WriteLine($"Producto cargado: {product} - Precio: ${price}");
 Console.ReadLine();
 
-//etapa 3 
+//etapa 3 - 4 - 5 
+
 const decimal DescuentoDiez = 0.10m;
 const decimal DescuentoCinco = 0.05m;
+const decimal DescuentoEfectivo = 0.10m;
+const decimal RecargoCredito = 0.15m;
 
 decimal total = 0;
 int cantidadProductos = 0;
@@ -70,6 +73,42 @@ do
             Console.WriteLine($"Subtotal: ${total}");
             Console.WriteLine($"Descuento aplicado: ${descuento}");
             Console.WriteLine($"Total con descuento: ${totalConDescuento}");
+            
+            int medioPago;
+            decimal totalFinal = totalConDescuento;
+
+            do
+            {
+                Console.WriteLine("Medio de pago:");
+                Console.WriteLine("1 - Efectivo");
+                Console.WriteLine("2 - Débito");
+                Console.WriteLine("3 - Crédito");
+
+                medioPago = int.Parse(Console.ReadLine());
+
+                switch (medioPago)
+                {
+                    case 1:
+                        totalFinal = totalConDescuento - (totalConDescuento * DescuentoEfectivo);
+                        break;
+
+                    case 2:
+                        totalFinal = totalConDescuento;
+                        break;
+
+                    case 3:
+                        totalFinal = totalConDescuento + (totalConDescuento * RecargoCredito);
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción inválida.");
+                        break;
+                }
+
+            }
+            while (medioPago < 1 || medioPago > 3);
+
+            Console.WriteLine($"Total final: ${totalFinal}");
 
             break;
 
@@ -80,6 +119,7 @@ do
 
 }
 while (opcion != 2);
+
 
 
 
